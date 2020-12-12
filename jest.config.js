4f2ff0c24af141d7a9fbbd952668e15922223a02
ac-greener <15929774304@163.com>
2020-12-12 14:50:03 +0800

    feature: 引入jest测试代码

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  verbose: true,
+  clearMocks: false,
+  collectCoverage: false,
+  reporters: ["default"],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleDirectories: ['node_modules'],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json',
+    },
+  },
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
+  },
+  //测试文件的路径
+  testMatch: ['<rootDir>/**/__tests__/**/*.test.(js|jsx|ts|tsx)'],
+  transform: {
+    "^.+test\\.(js|jsx)$": "babel-jest",
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ["<rootDir>test/setupTests.js"]
+}
