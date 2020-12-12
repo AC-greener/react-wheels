d4bb96e8f933ea432f9ba76327ef6cbba7e4b8e5
zhutongtong <zhutongtong@360.cn>
2020-12-12 12:52:41 +0800

    feature: 添加html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'production',
   entry: {
@@ -20,10 +20,10 @@ module.exports = {
       }
     ]
   },
-  // plugins: [
-  //   new HtmlWebpackPlugin({
-  //     template: 'index.html'
-  //   })
-  // ]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
 
 }
