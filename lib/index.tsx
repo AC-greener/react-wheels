2558b93f4093cd52820fd3825a19b70bb37ea741
ac-greener <15929774304@163.com>
2020-12-12 14:05:10 +0800

    feature: 拆分webpack dev和production配置

--- lib/index.tsx
+++ lib/index.tsx
@@ -3,5 +3,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 const a = 1
 console.log('hi')
-ReactDOM.render(<Button></Button>, document.body)
+ReactDOM.render(<Button></Button>, document.getElementById('root'))
 export {Button, a}
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,23 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  //配置模式 开发 或者生产
-  mode: 'development',
-  entry: {
-    index: './lib/index.tsx'
-  },
-  output: {
-    //操作系统路径的表示方式不同, path.resolve会根据操作系统的不同，吧当前目录和dist目录连接起来
-    path: path.resolve(__dirname, 'dist/lib'),
-    library: 'FUI',
-    //统一模块定义 umd, 统一(兼容)了commjs和 amd
-    libraryTarget: 'umd',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-},
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader'
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html'
-    })
-  ],
-  //外部的库，不打包
-  externals: {
-    react: {
-        commonjs: 'react',
-        commonjs2: 'react',
-        amd: 'react',
-        root: 'React',
+    entry: {
+        index: './lib/index.tsx'
     },
-    'react-dom': {
-        commonjs: 'react-dom',
-        commonjs2: 'react-dom',
-        amd: 'react-dom',
-        root: 'ReactDOM',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
     },
-}
+    output: {
+        path: path.resolve(__dirname, 'dist/lib'),
+        library: 'FUI',
+        libraryTarget: 'umd',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    },
 }
