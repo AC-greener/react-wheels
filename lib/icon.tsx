4194e7b7168ae06b3fdbb3ec4da24e9d2d5fd099
ac-greener <15929774304@163.com>
2020-12-13 16:55:54 +0800

    使用...props接收所有的props

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import './icon.scss'
 import './importicon.js'
-interface IconProps {
+
+interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string
 }
-const Icon:React.FunctionComponent<IconProps> = (props)  => {
+
+const Icon: React.FunctionComponent<IconProps> = (props: IconProps)  => {
+  console.log({...props})
   return (
-      <svg className='xui-icon'>
-        <use xlinkHref={`#${props.name}`}/>
-      </svg>
+    <svg className='xui-icon' {...props}>
+      <use xlinkHref={`#${props.name}`}/>
+    </svg>
   )
 }
 
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Button from './button'
 import Icon from './icon'
-
+function fn(e) {
+  console.log(e.target)
+}
 console.log('hi')
 ReactDOM.render(
 <div>
-  <Icon name='wechat'/>
+  <Icon onClick={fn} name='wechat'/>
   <Icon name='qq'/>
   <Icon name='360'/>
 </div>, document.getElementById('root'))
