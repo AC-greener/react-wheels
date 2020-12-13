1181010e4cc4b57c352ecb6fd19565d5191ad656
ac-greener <15929774304@163.com>
2020-12-13 16:09:45 +0800

    feature: 引入sass loader

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
+import './icon.scss'
 import './importicon.js'
 interface IconProps {
   name: string
 }
 const Icon:React.FunctionComponent<IconProps> = (props)  => {
   return (
-    <span>
-      <svg>
+      <svg className='xui-icon'>
         <use xlinkHref={`#${props.name}`}/>
       </svg>
-    </span>
   )
 }
 
