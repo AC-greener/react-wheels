b3a5e83acdfe6c8b8f43f416e43bef8aa32b7fcf
ac-greener <15929774304@163.com>
2020-12-13 17:30:35 +0800

    feature：使用classes计算className

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import './icon.scss'
 import './importicon.js'
-
+import classes from './helper/classes'
 interface IconProps extends React.SVGAttributes<SVGElement> {
-  name: string
+  name: string,
+  className?: string
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props: IconProps)  => {
-  console.log({...props})
+  const { className, name, ...restProps } = props
   return (
-    <svg className='xui-icon' {...props}>
-      <use xlinkHref={`#${props.name}`}/>
+    <svg className={ classes('xui-icon', className)} {...restProps}>
+      <use xlinkHref={`#${name}`}/>
     </svg>
   )
 }
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,13 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Button from './button'
 import Icon from './icon'
-function fn(e) {
-  console.log(e.target)
-}
-console.log('hi')
+
 ReactDOM.render(
 <div>
-  <Icon onClick={fn} name='wechat'/>
+  <Icon name='wechat'/>
   <Icon name='qq'/>
   <Icon name='360'/>
 </div>, document.getElementById('root'))
