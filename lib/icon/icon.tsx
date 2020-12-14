44b9e68321f1c62eb688456cdf4831021c99eb63
ac-greener <15929774304@163.com>
2020-12-14 20:55:04 +0800

    other: 移动icon目录

--- lib/icon/icon.tsx
+++ lib/icon/icon.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './icon.scss'
 import './importicon.js'
-import classes from './helper/classes'
+import classes from '../helper/classes'
 
 interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string,
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Button from './button'
-import Icon from './icon'
+import Icon from './icon/icon'
 
 ReactDOM.render(
 <div>
