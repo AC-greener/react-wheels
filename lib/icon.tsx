421d20aa94b2ae6cff8eda069acff91a98dd29c0
ac-greener <15929774304@163.com>
2020-12-14 13:25:43 +0800

    feature: icon可以接受color和size参数

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -2,18 +2,22 @@ import React from 'react'
 import './icon.scss'
 import './importicon.js'
 import classes from './helper/classes'
+
 interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string,
-  className?: string
+  color?: string
+  fontSize?: string
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props: IconProps)  => {
-  const { className, name, ...restProps } = props
+  const { className, color, fontSize, name, ...restProps } = props
+
   return (
-    <svg className={ classes('xui-icon', className)} {...restProps}>
+    <svg className={ classes('xui-icon', className, color ? `color-${color}`: '', fontSize? `size-${fontSize}`: '')} {...restProps}>
       <use xlinkHref={`#${name}`}/>
     </svg>
   )
 }
+
 
 export default Icon
--- lib/index.tsx
+++ lib/index.tsx
@@ -5,8 +5,17 @@ import Icon from './icon'
 
 ReactDOM.render(
 <div>
-  <Icon name='wechat'/>
-  <Icon name='qq'/>
-  <Icon name='360'/>
+  <h3>color</h3>
+  <Icon name="wechat"/>
+  <Icon color="primary" name="wechat"/>
+  <Icon color="secondary" name="wechat"/>
+  <Icon color="action" name="wechat"/>
+  <Icon color="disabled" name="wechat"/>
+  <Icon style={{ color: 'green' }} name="wechat"/>
+  <h3>size</h3>
+  <Icon fontSize="small" name="wechat"/>
+  <Icon name="wechat"/>
+  <Icon fontSize="large" name="wechat"/>
+  <Icon style={{ fontSize: 40 }} name="wechat"/>
 </div>, document.getElementById('root'))
 export { Button }
