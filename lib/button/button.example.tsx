e5805a046e1084091285463e669e609a4d4c0234
ac-greener <15929774304@163.com>
2020-12-28 01:12:33 +0800

    feat: 添加文本按钮样式

--- lib/button/button.example.tsx
+++ lib/button/button.example.tsx
@@ -8,25 +8,31 @@ const ButtonExample: React.FunctionComponent = () => {
         <h1>
           Contained Buttons 实心按钮
         </h1>
-        <Button>default</Button>
-        <Button color="primary">primary</Button>
-        <Button color="info">info</Button>
-        <Button color="green">green</Button>
-        <Button color="rose">rose</Button>
-        <Button color="purple">purple</Button>
-        <Button className="custom">custom css</Button>
+        <Button type="contained">default</Button>
+        <Button type="contained" color="primary">primary</Button>
+        <Button type="contained" color="info">info</Button>
+        <Button type="contained" color="green">green</Button>
+        <Button type="contained" color="rose">rose</Button>
+        <Button type="contained" color="purple">purple</Button>
+        <Button type="contained" className="custom">custom css</Button>
       </div>
       <div>
         <h1>
         Text Buttons 文本按钮
         </h1>
-        <Button >222</Button>
+        <Button >default</Button>
+        <Button color="primary">primary</Button>
+        <Button color="info">info</Button>
+        <Button color="green">green</Button>
+        <Button color="rose">rose</Button>
+        <Button color="purple">purple</Button>
+        <Button className="text-custom">custom css</Button>
       </div>
       <div>
         <h1>
         Outlined Buttons 描边按钮
         </h1>
-        <Button className="aaa">111</Button>
+        {/* <Button >default</Button> */}
       </div>
     </div>
   );
--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -5,12 +5,17 @@ import { scopedClassMaker } from '../helper/index'
 
 interface buttonProps extends React.HTMLAttributes<HTMLButtonElement> {
   color?: string
+  type?: string
 }
 const scopedClass = scopedClassMaker('fake-button')
 const Icon: React.FunctionComponent<buttonProps> = (props: buttonProps)  => {
-  const { className, color, ...restProps } = props
-
-  const classResult = classes(scopedClass(), className, "fake-radius", color)
+  const { className, color, type, ...restProps } = props
+  const classResult = classes(
+    scopedClass(),
+    `fake-${type ? type : "text"}`,
+    className, "fake-radius",
+    color ? `fake-${type ? type : "text"}-${color}`: ""
+  )
   return (
     <button type="button" className={classResult} {...restProps}>
       <span>{ props.children }</span>
