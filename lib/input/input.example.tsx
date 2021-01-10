39f24e37287ff99c9a2df43aaf4630023f97add5
ac-greener <15929774304@163.com>
2021-01-10 16:41:38 +0800

    feat: input接收onChange变成受控组件

--- lib/input/input.example.tsx
+++ lib/input/input.example.tsx
@@ -1,22 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Input from './input'
 import './input.example.scss'
 
 const InputExample = () => {
-
+  const [x, setX] = useState(1)
   return (
     <div>
       <div className="input-wrapper">
         <h1>Outlined </h1>
+        <Input variant="outlined" value={x} onChange={(e) => setX(e.target.value)}  />
         <Input variant="outlined" label="Outlined" />
         <Input variant="outlined" label="Outlined" defaultValue='Hello'  />
-        <Input variant="outlined" label="Disabled" defaultValue='hello' disabled />
+        <Input variant="outlined" label="Disabled" defaultValue='Disabled' disabled />
       </div>
       <div className="input-wrapper">
         <h1>Standard </h1>
+        <Input variant="standard" />
         <Input variant="standard" label="Standard" />
         <Input variant="standard" label="Standard" defaultValue='Hello'  />
-        <Input variant="standard" label="Standard" defaultValue='hello' disabled />
+        <Input variant="standard" label="Standard" defaultValue='Disabled' disabled />
       </div>
     </div>
   )
--- lib/input/input.tsx
+++ lib/input/input.tsx
@@ -7,7 +7,9 @@ interface inputProps extends React.HTMLAttributes<HTMLInputElement> {
   variant?: string
   label?: string
   disabled?: boolean
-  defaultValue?: string
+  value?: string | number
+  defaultValue?: string | number
+  onChange?: (e: any) => void
 }
 const scopedClass = scopedClassMaker('fake-input')
 const Input: React.FunctionComponent<inputProps> = (props: inputProps)  => {
@@ -19,10 +21,12 @@ const Input: React.FunctionComponent<inputProps> = (props: inputProps)  => {
     onBlur, 
     disabled,
     defaultValue,
+    value,
+    onChange,
     ...restProps 
   } = props
-  const labelRef: any = useRef(null)
-  const inputRef: any = useRef(null)
+  const labelRef = useRef<HTMLDivElement>(null)
+  const inputRef = useRef<HTMLInputElement>(null)
   const classResult = classes(
     scopedClass('root'),
     scopedClass(variant),
@@ -30,33 +34,38 @@ const Input: React.FunctionComponent<inputProps> = (props: inputProps)  => {
   )
   const handleFucus = (e) => {
     onFocus && onFocus(e)
-    labelRef.current.classList.toggle('fake-input-fucus')
+    labelRef.current!.classList.toggle('fake-input-fucus')
   }
   const handleBlur = (e) => {
     onBlur && onBlur(e)
-    labelRef.current.classList.toggle('fake-input-fucus')
+    labelRef.current!.classList.toggle('fake-input-fucus')
     if(e.target.value) {
-      labelRef.current.classList.add('fake-input-hasvalue')
+      labelRef.current!.classList.add('fake-input-hasvalue')
     } else {
-      labelRef.current.classList.remove('fake-input-hasvalue')
+      labelRef.current!.classList.remove('fake-input-hasvalue')
     }
   }
   useEffect(() => {
     if(defaultValue) {
-      labelRef.current.classList.add('fake-input-hasvalue')
-      inputRef.current.value = defaultValue
+      labelRef.current!.classList.add('fake-input-hasvalue')
     }
   })
 
+  const hanleInputChange = (e) => {
+    onChange && onChange(e)
+  }
   return (
     <div ref={ labelRef } className={ classResult }>
       <label className={ scopedClass("label") }  htmlFor="">{ label }</label>
       <div className={scopedClass("wrapper")}>
         <input
+          value= { value }
+          defaultValue={ defaultValue }
           ref={ inputRef }
           disabled={ disabled }
           onFocus={ handleFucus }
           onBlur={ (e) => handleBlur(e) }
+          onChange={ hanleInputChange }
           className={ scopedClass() } 
           { ...restProps } 
           type="text"
