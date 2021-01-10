6afad60f0fb712860eb3891008852a96fcf6b76e
ac-greener <15929774304@163.com>
2021-01-10 15:45:38 +0800

    style: 添加标准输入框的样式

--- lib/input/input.example.tsx
+++ lib/input/input.example.tsx
@@ -3,21 +3,20 @@ import Input from './input'
 import './input.example.scss'
 
 const InputExample = () => {
-  const handleChange = (e) => {
-    console.log(e)
-  }
+
   return (
     <div>
-      <div>
-        {/* <Input variant="standard" label="Standard" /> */}
-      </div>
-      <br/>
       <div className="input-wrapper">
         <h1>Outlined </h1>
         <Input variant="outlined" label="Outlined" />
-        <Input variant="outlined" label="Outlined" onChange={ (e) => handleChange(e) } />
         <Input variant="outlined" label="Outlined" defaultValue='Hello'  />
         <Input variant="outlined" label="Disabled" defaultValue='hello' disabled />
+      </div>
+      <div className="input-wrapper">
+        <h1>Standard </h1>
+        <Input variant="standard" label="Standard" />
+        <Input variant="standard" label="Standard" defaultValue='Hello'  />
+        <Input variant="standard" label="Standard" defaultValue='hello' disabled />
       </div>
     </div>
   )
