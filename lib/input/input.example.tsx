49ae6d9720be67a84e4113f858c827ff5e237655
ac-greener <15929774304@163.com>
2021-01-10 15:05:05 +0800

    feat: Input组件接收disabled和defaultValue属性

--- lib/input/input.example.tsx
+++ lib/input/input.example.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import Input from './input'
+import './input.example.scss'
 
 const InputExample = () => {
+  const handleChange = (e) => {
+    console.log(e)
+  }
   return (
     <div>
       <div>
-        <Input variant="standard" label="Standard" />
+        {/* <Input variant="standard" label="Standard" /> */}
       </div>
       <br/>
-      <div>
+      <div className="input-wrapper">
+        <h1>Outlined </h1>
         <Input variant="outlined" label="Outlined" />
+        <Input variant="outlined" label="Outlined" onChange={ (e) => handleChange(e) } />
+        <Input variant="outlined" label="Outlined" defaultValue='Hello'  />
+        <Input variant="outlined" label="Disabled" defaultValue='hello' disabled />
       </div>
     </div>
   )
--- lib/input/input.tsx
+++ lib/input/input.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useEffect } from 'react'
 import './input.scss'
 import classes from '../helper/classes'
 import { scopedClassMaker } from '../helper/index'
@@ -6,52 +6,69 @@ import { scopedClassMaker } from '../helper/index'
 interface inputProps extends React.HTMLAttributes<HTMLInputElement> {
   variant?: string
   label?: string
+  disabled?: boolean
+  defaultValue?: string
 }
 const scopedClass = scopedClassMaker('fake-input')
 const Input: React.FunctionComponent<inputProps> = (props: inputProps)  => {
-  const { className, variant, label, onFocus,onBlur, ...restProps } = props
-  const labelRef = useRef(null);
+  const {
+    className, 
+    variant, 
+    label, 
+    onFocus,
+    onBlur, 
+    disabled,
+    defaultValue,
+    ...restProps 
+  } = props
+  const labelRef: any = useRef(null)
+  const inputRef: any = useRef(null)
   const classResult = classes(
     scopedClass('root'),
     scopedClass(variant),
+    disabled ? scopedClass("disabled") : ''
   )
   const handleFucus = (e) => {
     onFocus && onFocus(e)
-    if(labelRef.current != null) {
-      labelRef.current.classList.toggle('fake-input-fucus')
-    }
+    labelRef.current.classList.toggle('fake-input-fucus')
   }
   const handleBlur = (e) => {
     onBlur && onBlur(e)
-    if(labelRef.current != null) {
-      labelRef.current.classList.toggle('fake-input-fucus')
-    }
+    labelRef.current.classList.toggle('fake-input-fucus')
     if(e.target.value) {
       labelRef.current.classList.add('fake-input-hasvalue')
     } else {
       labelRef.current.classList.remove('fake-input-hasvalue')
     }
   }
+  useEffect(() => {
+    if(defaultValue) {
+      labelRef.current.classList.add('fake-input-hasvalue')
+      inputRef.current.value = defaultValue
+    }
+  })
 
   return (
     <div ref={ labelRef } className={ classResult }>
-      <label  className="fake-input-label" htmlFor="">{ label }</label>
-      <div className={`fake-input-wrapper`}>
+      <label className={ scopedClass("label") }  htmlFor="">{ label }</label>
+      <div className={scopedClass("wrapper")}>
         <input
+          ref={ inputRef }
+          disabled={ disabled }
           onFocus={ handleFucus }
           onBlur={ (e) => handleBlur(e) }
-          className='fake-input' 
+          className={ scopedClass() } 
           { ...restProps } 
           type="text"
         />
       </div>
     </div>
-
   )
 }
 
 Input.defaultProps = {
-  variant: 'standard'
+  variant: 'standard',
+  disabled: false
 }
 
 
