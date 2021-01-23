1be86f111afced20eaf6adef470df54819547ea0
ac-greener <15929774304@163.com>
2021-01-23 15:29:06 +0800

    feat: totast可以出现在不同的位置

--- lib/toast/toast.example.tsx
+++ lib/toast/toast.example.tsx
@@ -1,24 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Toast from './toast'
-
+import { Button } from '../index'
 
 const ToastExample = () => {
   const handleClose = () => {
     console.log('close')
+    setVisible(false)
   }
+  const [visible, setVisible] = useState(false)
+  const [vertical, setVertical] = useState('top')
+  const [horizontal, setHorizontal] = useState('right')
   return (
     <div>
       <div>
         <h1>totast</h1>
+        <Button type='contained' onClick={() => {
+          setVisible(true)
+          setVertical('top')
+          setHorizontal('left')
+        }}>
+          TOP LEFT
+        </Button>
+        <Button type='contained' onClick={() => {
+          setVisible(true)
+          setVertical('top')
+          setHorizontal('center')
+        }}>
+          TOP CENTER
+        </Button>
+        <Button type='contained' onClick={() => {
+          setVisible(true)
+          setVertical('top')
+          setHorizontal('right')
+        }}>
+          TOP RIGHT
+        </Button>
+        <Button type='contained' onClick={() => {
+          setVisible(true)
+          setVertical('bottom')
+          setHorizontal('left')
+        }}>
+          BOTTOM LEFT
+        </Button>
+        <Button type='contained' onClick={() => {
+          setVisible(true)
+          setVertical('bottom')
+          setHorizontal('center')
+        }}>
+          BOTTOM CENTER
+        </Button>
+        <Button type='contained' onClick={() => {
+          setVisible(true)
+          setVertical('bottom')
+          setHorizontal('right')
+        }}>
+          BOTTOM RIGHT
+        </Button>
+
         <Toast 
-          visible={ true } 
+          visible={ visible } 
           message="Note archived" 
           onClose={ handleClose }         
           anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left',
+            vertical,
+            horizontal,
           }}
         />
+      
       </div>
     </div>
   )
--- lib/toast/toast.tsx
+++ lib/toast/toast.tsx
@@ -1,20 +1,38 @@
 import React from 'react'
 import './toast.scss'
 import classes from '../helper/classes'
-
+import { Icon } from '../index'
 interface ToastProps extends React.HTMLAttributes<HTMLDivElement> {
   visible: boolean
+  message?: string
+  onClose?: React.MouseEventHandler
+  anchorOrigin: {
+    vertical: string
+    horizontal: string
+  }
 }
 
 const Totast: React.FunctionComponent<ToastProps> = (props: ToastProps)  => {
-  const { className, ...restProps } = props
+  const { className, message, onClose, anchorOrigin, visible, ...restProps } = props
   const name = classes(
+    'fake-toast-root',
+    `fake-toast-${anchorOrigin.vertical}-${anchorOrigin.horizontal}`,
     className
   )
+  const handleClick: React.MouseEventHandler = (e) => {
+    onClose && onClose(e)
+  }
   return (
-  <div className={ name } { ...restProps }>
-    totast
-  </div>
+    visible ? 
+      <div className={ name } { ...restProps }>
+      <div className='fake-toast-message'>
+        { message }
+      </div>
+      <div className='fake-toast-action' onClick={ handleClick }>
+        <Icon name='close'/>
+      </div>
+    </div>
+    : null
   )
 }
 
