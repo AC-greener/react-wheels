e1f5650ce09e3ef373ac08f38b6efef464d059c9
ac-greener <15929774304@163.com>
2021-01-30 16:48:08 +0800

    feat: uploader接收headers

--- lib/uploader/uploader.example.tsx
+++ lib/uploader/uploader.example.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Uploader from './uploader'
 const UploaderExample = () => {
   const handleChange = (file) => {
-    console.log(file)
   }
   return (
     <div>
       <Uploader
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
         name="img"
+        headers={
+          {'authorization': 'authorization-text'}
+        }
         onChange={ handleChange }
       />
     </div>
--- lib/uploader/uploader.tsx
+++ lib/uploader/uploader.tsx
@@ -1,19 +1,27 @@
 import classes from '../helper/classes'
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import './uploader.scss'
 
 interface UploaderProps extends React.HTMLAttributes<HTMLDivElement> {
   name: string
-  action?: string
+  action: string
   headers?: {
     authorization: string,
   }
   method?: string
   onChange?:(object) => void
 }
+
+const createFormData = (name, file) => {
+  const formData = new FormData()
+  formData.append(name, file)
+  return formData
+}
+
 const Uploader: React.FunctionComponent<UploaderProps> = (props) => {
-  const { className, name, onClick, onChange, ...restProps} = props
+  const { className, name, onClick, method, action, headers, onChange, ...restProps} = props
   const inputElementRef = useRef<HTMLInputElement>(null)
+  const [file, setFile] = useState(null)
   const cname = classes(
     "fake-uploader-root",
     className
@@ -25,7 +33,22 @@ const Uploader: React.FunctionComponent<UploaderProps> = (props) => {
     // status: uploading done error removed
     const file = (e.target as any).files[0]
     file.status = "uploading"
+    setFile(file)
     onChange && onChange(file)
+    uploadFile()
+  }
+
+  const uploadFile = () => {
+    console.log(1111)
+    fetch(action, {
+      method,
+      body: createFormData(name, file),
+      headers: new Headers(headers)
+    })
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => console.error('Error:', error))
   }
   return (
     <div className={ cname } {...restProps} onClick={ handleClick } >
